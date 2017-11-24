f9909902bf68fc0e15f4e96b5002a78aa398d508
Acollie <alex@alexcollie.com>
2017-11-24 14:56:52 +0000

    a

--- q1.py
+++ q1.py
@@ -10,11 +10,9 @@ Time_Swim=float(0)
 Time_Cycle=float(0)
 Time_Running=float(0)
 
-if Time_Swim or Time_Cycle or Time_Running>0:
-    print "Error"
-    Time_Swim = float(0)
-    Time_Cycle = float(0)
-    Time_Running = float(0)
+if Swim_dis<0 or Time_Cycle<0 or Run_dis<0:
+    print "Error one or more of your entered input was less than 0 please try again."
+
 else:
 
     Time=float(0)
@@ -33,11 +31,17 @@ else:
  
 
     def Main():
+        print "\n \n"
+
+        print "swimming distance you entered:%.1f metres"%Swim_dis
+        print "For cycling distance you entered: %.1f metres"%Cycle_dis
+        print "For running distance you entered: %.1f metres"%Run_dis
+        print
         print "Discipline       Time Taken (s)"
         print "======"*4
-        print "Swim         %2.2f"%(Swim())
-        print "Cycling      %2.2f" % (Cycling())
-        print "Running      %2.2f" % (Running())
+        print "Swim         %10.2f"%(Swim())
+        print "Cycling      %10.2f" % (Cycling())
+        print "Running      %10.2f" % (Running())
 
         Time=Swim()+Cycling()+Running()
         print Time
