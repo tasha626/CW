f0192df3114e8f33ea6dfb183547b91b38b6150a
Acollie <alex@alexcollie.com>
2017-11-24 17:29:17 +0000

    Fri_remake

---
+++ q1_new.py
@@ -0,0 +1,41 @@
+
+id=['Bike','Swim','Running']
+speed=[1.77,14.66,5.1]
+distance=[]
+
+def input():
+    for i in range(len(id)):
+        distance.append(float(raw_input("Disance for "+id[i])))
+        if distance[i]<0:
+
+            print "Error disance cannot be less than 0"
+            exit(0)
+
+
+def Display():
+
+    print "======"*3
+
+def Calculation(disance,speed):
+    Time=float(disance/speed)
+    return Time
+
+
+
+def Main():
+    input()
+
+    for i in range(len(id)):
+        print
+        print "For",id[i],"distance you entered",distance[i],"Metres"
+    print "\n "*3
+    print "Discipline           Time Taken (s)"
+    print
+
+    for i in range(len(id)):
+        tmp=Calculation(float(distance[i]), float(speed[i]))
+        print "%10s %20.2f"%(id[i],tmp)
+
+
+
+Main()
