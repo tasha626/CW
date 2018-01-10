c82645978c74e9e226cf2102d3e94afb1d2a553c
tasha626 <34651287+tasha626@users.noreply.github.com>
2018-01-10 15:21:47 +0000

    Added comments

--- q1.py
+++ q1.py
@@ -1,8 +1,10 @@
+# Empty lists for distance & timetaken to allow user imput 
 id=['Swim','Bike','Running']
 speed=[1.72,14.66,5.1]
 distance=[]
 timetaken=[]
 
+# Created error message for user to prevent them imputting a number less than 0
 def input():
     for i in range(len(id)):
         distance.append(float(raw_input("Disance for "+id[i])))
@@ -11,17 +13,18 @@ def input():
             print "Error disance cannot be less than 0"
             exit(0)
 
-
+# Printing a line
 def Display():
 
     print "======"*3
 
+# Calculation for distance over speed to get Time
 def Calculation(disance,speed):
     Time=float(disance/speed)
     return Time
 
 
-
+# Printing out results with table setup & incorporating friendly message
 def Main():
     input()
 
@@ -36,7 +39,8 @@ def Main():
         tmp=Calculation(float(distance[i]), float(speed[i]))
         timetaken.append(tmp)
         print "%10s %15.2f"%(id[i],tmp)
-        
+        
+
 
 
     print ""
